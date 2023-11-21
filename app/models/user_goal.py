a57376ff1424ba74587ea6f46dbc931ad0bfd525
kmishmael <kmishmael@gmail.com>
2023-11-21 18:18:07 +0300

    chore: user goal model

---
+++ app/models/user_goal.py
@@ -0,0 +1,20 @@
+"""User goal model"""
+
+import models
+import enum
+from app import db
+
+class GoalType(enum.Enum):
+    """goal type enum definition"""
+    add = 'ADD'
+    lose = 'LOSE'
+    maintain = 'MAINTAIN'
+
+class UserGoal(db.Model()):
+    """user goal model"""
+    id = db.Column(db.Integer(), primary_key=True)
+    profile_id = db.Column(db.Text(), db.ForeignKey('profile.id'))
+    type = db.Column(db.Enum(GoalType))
+    target_cal = db.Column(db.Integer())
+    target_weight = db.Column(db.Integer())
+    duration = db.Column(db.Integer())
