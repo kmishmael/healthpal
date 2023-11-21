32082e69429d7bb8aea64f3d6ad808590b7615b8
kmishmael <kmishmael@gmail.com>
2023-11-21 18:18:07 +0300

    chore: user goal model

--- app/models/water_goal.py
+++ app/models/water_goal.py
@@ -1,2 +1,10 @@
+"""water goal model"""
 import models
 from app import db
+
+class WaterGoal(db.Model):
+    """Water goal model"""
+    id = db.Column(db.Integer(), primary_key=True)
+    profile_id = db.Column(db.Integer(), db.ForeignKey('profile.id'))
+    goal_cups = db.Column(db.Integer())
+    current_cups = db.Column(db.Integer())
