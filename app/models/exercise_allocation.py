fa4e2b6546895b7f0464f2421359d8cf4902765e
kmishmael <kmishmael@gmail.com>
2023-11-21 18:48:26 +0300

    chore: exercise allocation model

---
+++ app/models/exercise_allocation.py
@@ -0,0 +1,11 @@
+"""Exercise allocation"""
+
+import models
+from app import db
+
+class ExerciseAllocation(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    exercises_id = db.Column(db.Integer(), db.ForeignKey('exercises.id'))
+    reps = db.Column(db.Integer())
+    sets = db.Column(db.Integer())
+    exercise_id =db.Column(db.Integer(), db.ForeignKey('exercise.id'))
