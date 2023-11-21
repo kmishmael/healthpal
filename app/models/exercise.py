5b775e8d490d0ec045335aa5d48c0960a50f5b38
kmishmael <kmishmael@gmail.com>
2023-11-21 18:26:30 +0300

    chore: exercises model

---
+++ app/models/exercise.py
@@ -0,0 +1,14 @@
+"""Exercise model"""
+
+import models
+from app import db
+
+class Exercise(db.Model):
+    """exercises model"""
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.Text())
+    type = db.Column(db.Text())
+    sets = db.Column(db.Integer())
+    reps = db.Column(db.Integer())
+    calories = db.Column(db.Integer())
+    time =  db.Column(db.Integer())
