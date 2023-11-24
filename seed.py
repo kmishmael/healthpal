194670ff1aceb7ae9b4e203125f37be13ffa8b81
kmishmael <kmishmael@gmail.com>
2023-11-24 23:34:34 +0300

    chore: user api testing

--- seed.py
+++ seed.py
@@ -2,6 +2,7 @@ from faker import Faker
 from app.instance import app
 from app.db import db
 from app.models.user import User
+from app.models.user import IsVerified
 import random
 
 fake = Faker()
@@ -13,17 +14,22 @@ with app.app_context():
     User.query.delete()
     new_users = []
     for _ in range(20):
-        id = random.randint(1000, 10000)
-        name = fake.name()
-        email = fake.email()
-        profile_photo = 'https://avatars.githubusercontent.com/u/66499851'
-        email_verified = True
-        age = random.randint(18, 35)
-        height = 183
-        weight = 193
-        gender = 'MALE'
-        target_calories = random.randint(2000, 2500)
 
-    db.session.add_all(new_users)
+        new_user = {
+            'id': random.randint(1000, 10000),
+            'name': fake.name(),
+            'email': fake.email(),
+            'profile_photo': 'https://avatars.githubusercontent.com/u/66499851',
+            'email_verified': IsVerified.true,
+            'age': random.randint(18, 35),
+            'height': 183,
+            'weight': 193,
+            'target_calories': random.randint(2000, 2500)
+        }
+
+        new_user = User(**new_user)
+        db.session.add(new_user)
+
+    #db.session.add_all(new_users)
     db.session.commit()
     print('Seeding Successful')
