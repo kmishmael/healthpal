025c5dc93de6cf65b312d76b4c52b091b136b239
kmishmael <kmishmael@gmail.com>
2023-11-25 20:49:13 +0300

    chore: login and check current user logic

--- app/api/auth/business.py
+++ app/api/auth/business.py
@@ -1,12 +1,13 @@
 """Business logic for /auth API endpoints."""
 from http import HTTPStatus
-import jwt
 from flask import current_app, jsonify
 from flask_restx import abort
-
 from app import db
 from app.models.user import User
-
+from app.api.auth.decorators import token_required
+from app.util.datetime_util import (
+    remaining_fromtimestamp, format_timespan_digits
+)
 
 def process_registration_request(email, password):
     if User.find_by_email(email):
@@ -15,14 +16,34 @@ def process_registration_request(email, password):
     db.session.add(new_user)
     db.session.commit()
     access_token = new_user.encode_access_token()
+    return _create_auth_successful_response(
+        token=access_token.decode(),
+        status_code=HTTPStatus.CREATED,
+        message="successfully registered",
+    )
+
+
+def process_login_request(email, password):
+    user = User.find_by_email(email)
+    if not user or not user.check_password(password):
+        abort(HTTPStatus.UNAUTHORIZED, "email or password does not match", status="fail")
+    access_token = user.encode_access_token()
+    return _create_auth_successful_response(
+        token=access_token.decode(),
+        status_code=HTTPStatus.OK,
+        message="successfully logged in",
+    )
+
+
+def _create_auth_successful_response(token, status_code, message):
     response = jsonify(
         status="success",
-        message="successfully registered",
-        access_token=access_token,
+        message=message,
+        access_token=token,
         token_type="bearer",
         expires_in=_get_token_expire_time(),
     )
-    response.status_code = HTTPStatus.CREATED
+    response.status_code = status_code
     response.headers["Cache-Control"] = "no-store"
     response.headers["Pragma"] = "no-cache"
     return response
@@ -33,3 +54,11 @@ def _get_token_expire_time():
     token_age_m = current_app.config.get("TOKEN_EXPIRE_MINUTES")
     expires_in_seconds = token_age_h * 3600 + token_age_m * 60
     return expires_in_seconds if not current_app.config["TESTING"] else 5
+
+@token_required
+def get_logged_in_user():
+    public_id = get_logged_in_user.public_id
+    user = User.find_by_public_id(public_id)
+    expires_at = get_logged_in_user.expires_at
+    user.token_expires_in = format_timespan_digits(remaining_fromtimestamp(expires_at))
+    return user
--- app/api/auth/endpoints.py
+++ app/api/auth/endpoints.py
@@ -3,11 +3,11 @@ from http import HTTPStatus
 
 from flask_restx import Namespace, Resource
 
-from app.api.auth.dto import auth_reqparser
-from app.api.auth.business import process_registration_request
+from app.api.auth.dto import auth_reqparser, user_model
+from app.api.auth.business import process_registration_request, process_login_request, get_logged_in_user
 
 auth_ns = Namespace(name="auth", validate=True)
-
+auth_ns.models[user_model.name] = user_model
 
 @auth_ns.route("/register", endpoint="auth_register")
 class RegisterUser(Resource):
@@ -24,3 +24,32 @@ class RegisterUser(Resource):
         email = request_data.get("email")
         password = request_data.get("password")
         return process_registration_request(email, password)
+
+@auth_ns.route("/login", endpoint="auth_login")
+class LoginUser(Resource):
+    """Handles HTTP requests to URL: /api/v1/auth/login."""
+
+    @auth_ns.expect(auth_reqparser)
+    @auth_ns.response(int(HTTPStatus.OK), "Login succeeded.")
+    @auth_ns.response(int(HTTPStatus.UNAUTHORIZED), "email or password does not match")
+    @auth_ns.response(int(HTTPStatus.BAD_REQUEST), "Validation error.")
+    @auth_ns.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), "Internal server error.")
+    def post(self):
+        """Authenticate an existing user and return an access token."""
+        request_data = auth_reqparser.parse_args()
+        email = request_data.get("email")
+        password = request_data.get("password")
+        return process_login_request(email, password)
+
+@auth_ns.route("/user", endpoint="auth_user")
+class GetUser(Resource):
+    """Handles HTTP requests to URL: /api/v1/auth/user."""
+
+    @auth_ns.doc(security="Bearer")
+    @auth_ns.response(int(HTTPStatus.OK), "Token is currently valid.", user_model)
+    @auth_ns.response(int(HTTPStatus.BAD_REQUEST), "Validation error.")
+    @auth_ns.response(int(HTTPStatus.UNAUTHORIZED), "Token is invalid or expired.")
+    @auth_ns.marshal_with(user_model)
+    def get(self):
+        """Validate access token and return user info."""
+        return get_logged_in_user()
