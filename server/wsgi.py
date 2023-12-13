77768c4d5217ed65431648c9685fa643ecb9e9eb
kmishmael <kmishmael@gmail.com>
2023-12-14 01:43:31 +0300

    chore: remove env variables

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,10 +1,8 @@
 import os
-from dotenv import load_dotenv
 from server.app import create_app
 
 
 app = create_app(os.getenv("FLASK_ENV", "development"))
 
-load_dotenv()
 if __name__ == "__main__":
     app.run()
