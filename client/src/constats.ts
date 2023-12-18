61a40dbdb2c22e91eca3b1fcc9c707be6da9e228
kmishmael <kmishmael@gmail.com>
2023-12-18 22:44:22 +0300

    chore: link to deployed backend api

--- client/src/constats.ts
+++ client/src/constats.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:5000/api/v1/'
+export const API_URL = 'http://54.82.200.245/api/v1/'
 export const DAYS_OF_WEEK = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
